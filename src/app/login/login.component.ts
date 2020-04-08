a269653d0fac4652e9a09277bb75d730f41ee820
Eduardo <desenvolvimento03.onnetmais@gmail.com>
2020-04-08 13:43:41 -0300

    Ajuste da tela de login, adicionado função 'show password'

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,5 @@
+import { Login } from './../models/login';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private passwordVisible: boolean = false;
+  private typeInput: String = "password"
+
+  private loginForm: FormGroup;
+  private login = new Login();
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      username: [this.login.username],
+      password: [this.login.password],
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  showPassword() {
+    this.passwordVisible = !this.passwordVisible;
+
+    if (this.passwordVisible)
+      this.typeInput = "text";
+
+    else
+      this.typeInput = "password";
+  }
+
+  onSubmit() {
+    const loginFormValue = this.loginForm.value;
+    const rememberLogin: any = document.getElementById('remember')
+
+      console.log(loginFormValue);
+      console.log(rememberLogin.checked);
+  }
 }
