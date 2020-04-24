abe65e8af7109dc0d14d66bf1de8494bb504d5e8
Eduardo <desenvolvimento03.onnetmais@gmail.com>
2020-04-24 16:58:51 -0300

    Configuração da rota global do banco da aplicação

--- src/app/global.ts
+++ src/app/global.ts
@@ -3,5 +3,5 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalVariables {
     // token: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjMsImlhdCI6MTU3OTg4OTU0Mn0.i9XJNtpHKFn-sDhvP1DtQsNwb2W7JODDXc69lxJrHUY'
-    baseURL = 'http://127.0.0.1:3333';
+    baseURL = 'http://177.85.0.28:7200';
 }
