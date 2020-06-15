cb06b196d776a01704f3e59e6b52e03e8dcbe7af
Eduardo <desenvolvimento03.onnetmais@gmail.com>
2020-06-15 09:46:54 -0300

    Adicionado filtro no Perfil de Usuário

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,22 +1,26 @@
+import { GlobalFunctions } from './../../global';
+import { Search } from './../../models/Search';
+import { SearchService } from './../../services/search.service';
+import { DateAdapter } from '@angular/material/core';
 import { User } from './../../models/User';
 import { UserService } from './../../services/user.service';
-import { Search } from './../../models/Search';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { InterviewsCount } from './../../models/InterviewsCount';
-import { Chart } from 'chart.js';
-import { parseOptions, chartOptions, chartQuestions } from './../../variables/charts';
-import { SearchService } from './../../services/search.service';
-import { AnswerService } from './../../services/answer.service';
 import { InterviewService } from './../../services/interview.service';
-import { GlobalFunctions } from './../../global';
 import { Component, OnInit, ViewChild } from '@angular/core';
 
-interface DataChart {
-  id: Number;
-  quest: String;
-  label: string;
-  values: number[];
+interface DataMatTable {
+  data: InterviewsCount[];
+  displayedColumns: string[];
+  dataSource: MatTableDataSource<InterviewsCount>;
+  total: number;
+  loading: boolean;
+  filters: {
+    search: Search;
+    beginDate: Date;
+    endDate: Date;
+  }
 }
 
 @Component({
@@ -27,46 +31,82 @@ interface DataChart {
 export class UserProfileComponent implements OnInit {
   @ViewChild('sortCity', { static: true }) private sortCity: MatSort;
 
+  private matTable: DataMatTable = {} as DataMatTable;
+  private user: User = { permission: {} } as User;
+  private searches: Search[] = [];
+
   private displayedColumnsCity: string[] = ['name', 'count'];
   private dataSourceCity: MatTableDataSource<InterviewsCount>;
+
   private matCityLoading = true;
   private totalInterviews: Number = 0;
 
-  private user: User = {
-    id: 0,
-    name: '',
-    username: '',
-    password: '',
-    permission_id: 0,
-    active: true,
-    created_at: null,
-    updated_at: null,
-    permission: {
-      id: 0,
-      type: '',
-      active: true,
-      created_at: null,
-      updated_at: null
-    },
-  };
+  city;
+  beginDate;
+  endDate;
 
   constructor(
     private userService: UserService,
+    private searchService: SearchService,
     private interviewService: InterviewService,
+    private functions: GlobalFunctions,
+    private adapter: DateAdapter<any>
   ) {
-
-    parseOptions(Chart, chartOptions());
+    this.matTable = {
+      data: [],
+      dataSource: new MatTableDataSource([]),
+      displayedColumns: ['name', 'count'],
+      total: 0,
+      loading: true,
+      filters: {
+        search: null,
+        beginDate: null,
+        endDate: null
+      }
+    }
   }
 
   ngOnInit(): void {
+    this.adapter.setLocale('pt');
+
     this.refresh();
   }
 
   async refresh() {
+    this.refreshSearches();
+
+    this.refreshMatTable();
+
+    this.refreshUser();
+
     this.refreshMatTables().then(() => {
       this.matCityLoading = false;
     });
+  }
 
+  refreshMatTable(params?: string) {
+    const userId = sessionStorage.getItem('userId');
+
+    this.interviewService.groupByCity(`active=1&user=${userId}`, params).subscribe(
+      data => {
+        this.matTable.data = data;
+        this.matTable.dataSource.data = data;
+        this.matTable.dataSource.sort = this.sortCity;
+
+        this.matTable.total = this.matTable.data
+          .map(c => c.count).reduce((acc, value: any) => acc + value, 0);
+
+        this.matTable.loading = false;
+      },
+      err => {
+        this.matTable.loading = false;
+
+        console.log(err);
+      }
+    )
+  }
+
+  refreshUser() {
     this.userService.getById(parseInt(sessionStorage.getItem('userId'))).subscribe(
       user => {
         this.user = user;
@@ -75,6 +115,36 @@ export class UserProfileComponent implements OnInit {
         console.log(err);
       }
     );
+  }
+
+  refreshSearches() {
+    this.searchService.get().subscribe(
+      suc => {
+        this.searches = suc;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  filterMatTable() {
+    const toDate = this.functions.dateConverter;
+    var params: string = "";
+
+    if (this.matTable.filters.search)
+      params += "search=" + this.matTable.filters.search.id + "&";
+
+    if (this.matTable.filters.beginDate)
+      params += "begin=" + toDate(this.matTable.filters.beginDate) + "&";
+
+    if (this.matTable.filters.endDate)
+      params += "end=" + toDate(this.matTable.filters.endDate) + "&";
+
+    if (params)
+      params = params.slice(0, -1);
+
+    this.refreshMatTable(params);
   }
 
   async refreshMatTables() {
